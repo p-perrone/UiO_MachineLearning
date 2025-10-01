abd2e9962a437149d38795526f3be8552cf1a6ae
Pietro Perrone <pietro4061.perrone@gmail.com>
2025-10-01 23:35:50 +0200

    Update functions.py

--- ml_project1/functions.py
+++ ml_project1/functions.py
@@ -4,6 +4,19 @@
 #---------------------------------
 # author: Pietro Perrone
 #
+""" 
+=====================
+ml_project1.functions
+=====================
+This module contains functions for:
+    - Generating the Runge function with or without noise
+    - Creating a design matrix for polynomial regression
+    - Computing optimal parameters for Ordinary Least Squares (OLS) and Ridge (L2) regression
+    - Performing linear regression predictions
+    - Standard scaling of data
+    - Gradient descent optimization with different methods (AdaGrad, RMSprop, Adam)
+    - Lasso regression using gradient descent with L1 regularization
+"""
 
 #--------------------------------
 # dependencies
@@ -19,9 +32,11 @@ from sklearn.preprocessing import MinMaxScaler, StandardScaler, Normalizer
 import autograd.numpy as np
 from autograd import grad
 
+
 #--------------------------------
-# FIRST PART: PREPROCESSING AND OLS
+# FIRST PART: PREPROCESSING, OLS AND RIDGE
 #--------------------------------
+
 
 # Runge function
 def Runge(x, noise=True, noisescale=1):
@@ -40,8 +55,9 @@ def Runge(x, noise=True, noisescale=1):
 
     return y
 
+
 # OLS implementation
-def polynomial_features(x, p, intercept=True):
+def polynomial_features(x, p, intercept=True):  
     """ Computes the design matrix for linear regression.
         Parameters:
         :: x (array) = input dataset
@@ -63,6 +79,7 @@ def polynomial_features(x, p, intercept=True):
 
     return X
 
+
 def OLS_params(X, y):
     """ Computes optimal parameters for ordinary least squares regression.
         Parameters:
@@ -71,3 +88,215 @@ def OLS_params(X, y):
     """
     return np.linalg.pinv(X.T @ X) @ X.T @ y
 
+
+def Ridge_params(X, y, lbda):
+    """ Computes optimal parameters for Ridge regression.
+        Parameters:
+        :: X (matrix) = design matrix obtained with polynomial_features(x, p, intercept)
+        :: y (array) = true value to be modeled
+        :: lmbda (scalar) = penalty coefficient
+    """
+    return np.linalg.inv(X.T @ X + lbda * np.identity(X.shape[1])) @ X.T @ y
+
+
+def linear_regression(X, beta):
+    """ Computes the regression curve (predicted y values).
+        Parameters:
+        :: X (matrix) = design matrix
+        :: beta (array) = optimal OLS parameters obtained with OLS_params(X, y)
+    """
+    return X @ beta
+
+
+# standard scaling
+def standard_scaler(a, centering=True, stdscaling=True):
+    """ Scales input array a.
+        Parameters:
+        :: a (array) = input 1D array
+        :: centering (bool) = substracts a.mean()
+        :: scaling (bool) = divides by a.std()
+    """
+    if not isinstance(a, np.ndarray):
+        raise TypeError("'a' must be a 1D array")
+    if not isinstance(centering, bool):
+        raise TypeError("'centering' must be boolean (True or False)")
+    if not isinstance(stdscaling, bool):
+        raise TypeError("'stdscaling' must be boolean (True or False)")
+
+    if centering:
+        centered = a - a.mean()
+    else:
+        centered = a
+
+    if stdscaling:
+        scaled_a = centered / a.std()
+    else:
+        scaled_a = centered
+        
+    return scaled_a
+
+
+#--------------------------------
+# SECOND PART: GRADIENT DESCENT AND LASSO
+#--------------------------------
+
+
+# defining cost function for OLS, Ridge and Lasso
+def cost_function(y, X, theta, regression_method='OLS', lbda=0.1):
+    """ Cost function for OLS, Ridge or Lasso regression.
+        Parameters:
+        :: y (array) = true value to be modeled
+        :: X (matrix) = design matrix obtained with polynomial_features(x, p, intercept)
+        :: regression_method (str) = 'OLS', 'Ridge' or 'Lasso'
+        :: lbda (scalar) = regularization parameter (only for Ridge and Lasso)
+    """
+    if regression_method == 'OLS':
+        return np.sum((y - X @ theta)**2)
+    
+    elif regression_method == 'Ridge':
+        return np.sum((y - X @ theta)**2) + lbda * np.sum(theta**2)
+    
+    elif regression_method == 'Lasso':
+        return np.sum((y - X @ theta)**2) + lbda * np.sum(np.abs(theta))
+    
+    else:
+        raise ValueError(f"`regression_method` must be 'OLS', 'Ridge' or 'Lasso', not {regression_method}")
+
+
+# optimal parameters with simple gradient descent
+def theta_gd(X, y, eta, regression_method='OLS', lbda=1, iterations=2000, converge = 1e-8):
+    """ Computes optimal parameters for ordinary least squares regression with gradient descent.
+        Parameters:
+        :: X (matrix) = design matrix obtained with polynomial_features(x, p, intercept)
+        :: y (array) = true value to be modeled
+        :: eta (scalar) = learning rate
+        :: regression_method (str) = 'OLS' or 'Ridge'
+        :: lbda (scalar) = regularization parameter (only for Ridge)
+        :: iterations (int) = maximum number of iterations
+        :: converge (scalar) = convergence criterion
+    """
+    # error for wrong regression_method input
+    if regression_method not in ['OLS', 'Ridge', 'Lasso']:
+        raise ValueError("regression_method must be 'OLS', 'Ridge' or 'Lasso'")
+
+    # defining cost function
+    if regression_method == 'Ridge':
+        cost = lambda theta: cost_function(y, X, theta, regression_method='Ridge', lbda=lbda)
+    elif regression_method == 'Lasso':
+        cost = lambda theta: cost_function(y, X, theta, regression_method='Lasso', lbda=lbda)
+    else:
+        cost = lambda theta: cost_function(y, X, theta)
+
+    # initialize random theta
+    theta = np.random.randn(X.shape[1], 1)
+
+    # gradient descent loop
+    for k in range(iterations):
+        gradient = grad(cost)(theta).reshape(-1, 1)  # compute gradient with autograd
+        theta -= eta * gradient
+
+        # convergence of the algorithm
+        gradient_norm = np.linalg.norm(gradient)
+        if eta * gradient_norm <= converge:
+            print(f"Stop at epsilon = {gradient_norm:.2e}, iteration = {k}")
+            break
+
+    return theta    
+
+
+# optimal parameters with momentum gradient descent and different eta update methods
+def theta_gd_mom(X, y, regression_method='OLS', eta=1e-3, eta_update_method='Simple_momentum', lbdaRidge=0.1, 
+                lbdaLasso=0.1, momentum=0.9, iterations=2000, converge = 1e-8):
+    """ Computes optimal parameters for ordinary least squares regression with momentum gradient descent.
+        Parameters:
+        :: X (matrix) = design matrix obtained with polynomial_features(x, p, intercept)
+        :: y (array) = true value to be modeled
+        :: regression_method (str) = 'OLS', 'Ridge' or 'Lasso'
+        :: eta (scalar) = learning rate - default 1e-3 (optimal from experiments results)
+        :: lbdaRidge (scalar) = penalty coefficient for Ridge regression
+        :: lbdaLasso (scalar) = penalty coefficient for Lasso regression
+        :: momentum (scalar) = momentum coefficient for momentum gradient descent
+        :: eta_update_method (str) = 'AdaGrad', 'RMSprop', 'ADAM' or 'Simple_momentum'
+        :: iterations (int) = maximum number of iterations
+        :: converge (scalar) = convergence criterion
+    """
+
+    # error for wrong input
+    if regression_method != 'OLS' and regression_method != 'Ridge' and regression_method != 'Lasso':
+        raise ValueError(f"'regression_method' must be either 'OLS' or 'Ridge' and not {regression_method}")
+    if eta_update_method not in ['AdaGrad', 'RMSprop', 'ADAM', 'Simple_momentum']:
+        raise ValueError(f"'eta_update_method' must be either 'AdaGrad', 'RMSprop', 'ADAM' or 'Simple_momentum' and not {eta_update_method}")
+
+    # initialize random theta and velocity
+    theta = np.random.randn(X.shape[1], 1)
+    velocity = np.zeros((X.shape[1], 1))
+
+    # common params
+    delta = 1e-8    # to avoid division by zero
+    t = 0           # time step for Adam
+    Giter = 0.0     # initialize sum of squared gradients for AdaGrad, RMSprop and Adam
+
+    # defining cost function
+    if regression_method == 'OLS':
+        cost = lambda theta: cost_function(y, X, theta)
+    elif regression_method == 'Ridge':
+        cost = lambda theta: cost_function(y, X, theta, regression_method='Ridge', lbda=lbdaRidge)
+    elif regression_method == 'Lasso':
+        cost = lambda theta: cost_function(y, X, theta, regression_method='Lasso', lbda=lbdaLasso)
+
+    for k in range(iterations):
+        gradient = grad(cost)(theta).reshape(-1, 1)  # compute gradient with autograd
+        if np.any(np.isnan(gradient)) or np.any(np.isinf(gradient)):
+            print(f"NaN in gradient at iteration {k}")
+            print("Gradient stats:", np.min(gradient), np.max(gradient))
+            break
+
+        # update theta according to the chosen method
+        if eta_update_method == 'Simple_momentum':
+            velocity = momentum * velocity - eta * gradient     # simple momentum update
+            update = velocity
+            theta += update
+        
+        elif eta_update_method == 'AdaGrad': 
+            # AdaGrad tracks the sum of the squares of the previous gradients
+            Giter += gradient**2    # square the gradient
+            adjusted_eta = eta / (delta + np.sqrt(Giter))
+            update = adjusted_eta * gradient
+            theta -= update
+
+        elif eta_update_method == 'RMSprop':
+            # RMSprop modificates AdaGrad by useing a moving average of squared gradients to normalize the gradient
+            beta = 0.99     # discount factor that controls the averaging time of the second moment (variance of the gradient)
+            Giter = beta * Giter + (1 - beta) * gradient**2
+            adjusted_eta = eta / (delta + np.sqrt(Giter))
+            update = adjusted_eta * gradient
+            theta -= update
+
+        elif eta_update_method == 'ADAM':
+            # Adam (Adaptive Moment Estimation) is an extension to RMSprop that also takes 
+            # into account the moving average of the gradient itself
+            beta1 = 0.9     # decay rate for the first moment estimate (velocity = weighted average of the past gradients)
+            beta2 = 0.99    # decay rate for the second moment estimate (Giter = weighted average of the past squared gradients)
+            t += 1          # time step
+            
+            # update first moment estimate
+            velocity = beta1 * velocity + (1 - beta1) * gradient
+            # update second moment estimate
+            Giter = beta2 * Giter + (1 - beta2) * gradient**2
+            
+            # compute bias-corrected first moment estimate
+            velocity_corrected = velocity / (1 - beta1**t)
+            # compute bias-corrected second moment estimate
+            Giter_corrected = Giter / (1 - beta2**t)
+
+            adjusted_eta = eta / (delta + np.sqrt(Giter_corrected))
+            update = adjusted_eta * velocity_corrected
+            theta -= update
+
+        # convergence of the algorithm
+        update_norm = np.linalg.norm(update)
+        if update_norm <= converge:
+            print(f"Stop at epsilon = {update_norm:.2e}, iteration = {k}")
+            break
+
+    return theta
