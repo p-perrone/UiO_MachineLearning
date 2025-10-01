14baa0674c61d6076890d1843355323c6d7c1da7
Pietro Perrone <pietro4061.perrone@gmail.com>
2025-10-01 23:04:13 +0200

    Update setup.py

--- ml_project1/setup.py
+++ ml_project1/setup.py
@@ -1,10 +1,8 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="ml-project1",
+    name="ml_project1",
     version="0.1.0",
-    packages=find_packages(),
-    description="UiO Machine Learning projects",
-    package_dir={'': '..'},  # Look for packages in parent directory
-    py_modules=['ml_project1'],
+    py_modules=["functions"],
+    description="ML Project 1",
 )
